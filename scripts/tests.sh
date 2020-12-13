d28a0740da97e95a919c2c1de6834715c95569b5
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-13 22:57:35 +0300

    2

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
-export GTEST_COLOR=1
-CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
-CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
-CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/gcc-pic-cxx17.cmake'"
-CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
-cmake -H. -B_builds $CMAKE_OPTS
-cmake --build _builds
-cmake --build _builds --target test -- ARGS="--verbose"
+export CTEST_OUTPUT_ON_FAILURE=true
+./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --test --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
+./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target gcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
+./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target lcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
+gcovr -r  .
+
+REPORT_DATA=$(gcovr -r  . | base64 | tr -d '\n')
+POST_DATA="{\"report\": \"$REPORT_DATA\", \"slug\": \"$TRAVIS_REPO_SLUG\", \"head_branch\": \"$TRAVIS_BRANCH\", \"head_sha\": \"$TRAVIS_COMMIT\"}"
+if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
+curl -H "Content-Type: application/json" -d "$POST_DATA" -X POST http://borodin.dev.bmstu.cloud/api/coverage/
+fi
